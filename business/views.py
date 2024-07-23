f3d96de0151ee9c4581d96b04bc0bacbd812337f
pravison <pravisojuma087@gmail.com>
2024-07-23 22:08:26 +0300

    saving changes 4

--- business/views.py
+++ business/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render 
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from . import functions
+from .functions import sendWhatsappMessage
 import json
 
 # Create your views here.
@@ -25,6 +25,25 @@ def whatsappWebhook(request):
         
     if request.method == 'POST':
         data = json.loads(request.body)
-        print(data)
+        #print(data)
+        if 'object' in data and 'entry' in data:
+            if data['object'] == 'whatsapp_business_account':
+                try:
+                    for entry in data['entry']:
+                        phoneNumber = entry['changes'][0]['value']['metadata']['display_phone_number']
+                        phoneId =  entry['changes'][0]['value']['metadata']['phone_number_id']
+                        profileName = entry['changes'][0]['value']['contacts'][0]['profile']['name']
+                        whatsAppId =  entry['changes'][0]['value']['contacts'][0]['wa_id']
+                        fromId =  entry['changes'][0]['value']['messages'][0]['from']
+                        messageId = entry['changes'][0]['value']['messages'][0]['id']
+                        timestamp = entry['changes'][0]['value']['messages'][0]['timestamp']
+                        text = entry['changes'][0]['value']['messages'][0]['text']['body']
+
+                        phoneNumber = '254740562740'
+                        message = 'RE {} was received'.format(text)
+                        sendWhatsappMessage(phoneNumber, message)
+                except:
+                    pass
+
         return  HttpResponse('success', status=403)
         
