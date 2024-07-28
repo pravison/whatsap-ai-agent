e09a712878c1ca08e80c9b7a6adf6fb2db9479c7
pravison <pravisojuma087@gmail.com>
2024-07-28 17:47:12 +0300

    adding

--- business/views.py
+++ business/views.py
@@ -88,8 +88,8 @@ def whatsappWebhook(request):
                             text = messages[0].get('text', {}).get('body')
 
                             # Send the response once per incoming message
-                            message = '{}'.format(text)
-                            sendWhatsappMessage(fromId, message)
+                            sendWhatsappMessage(fromId, f'AI 🧠 is working on it ...')
+                            executor.submit( handleWhatsappCall, fromId , text)
                             break
             except Exception as e:
                 print(f"Error processing webhook data: {e}")
